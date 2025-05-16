3d9d973528f2658d772ed343929bbc0c1f357c98
johnvesslyalti <altijohnvessly@gmail.com>
2025-05-16 11:31:55 +0530

    feat: implement movie shuffling and update movie display with card components

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@
 import { useEffect, useState } from "react";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
     const [movies, setMovies] = useState([]);
@@ -17,12 +19,22 @@ export default function Page() {
         }
     };
 
+    const shuffleArray = (array) => {
+        const shuffled = [...array];
+        for (let i = shuffled.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+        }
+        return shuffled;
+    }
+
     useEffect(() => {
         const fetchMovies = async () => {
             try {
                 const response = await fetch(url, options);
                 const data = await response.json();
-                setMovies(data.results);
+                const shuffledMovies = shuffleArray(data.results);
+                setMovies(shuffledMovies);
                 setLoading(false);
             } catch(error) {
                 console.error(error);
@@ -41,14 +53,21 @@ export default function Page() {
                     <div className="absolute w-5 h-5 top-1/2 left-1/2 border-2 border-t-sky-500 animate-spin rounded-xl"></div>
                 )}
                 {movies.map((movie) => (
-                    <div key={movie.id}>
-                        <img 
-                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                        alt={movie.title}
-                        className="rounded"
-                        />
-                        <h2>{movie.title}</h2>
-                    </div>
+                    <Card className="flex flex-col h-[400px] hover:scale-102" key={movie.id}>
+                        <CardHeader className="flex-shrink-0 p-5">
+                            <img 
+                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                            alt={movie.title}
+                            className="object-cover rounded h-48 w-full"
+                            />
+                        </CardHeader>
+                        <CardContent className="flex flex-grow justify-center items-center overflow-hidden line-clamp-2">
+                            <p>{movie.title}</p>
+                        </CardContent>
+                        <CardFooter className="flex w-full justify-center items-center">
+                            <Button>Add to WatchList</Button>
+                        </CardFooter>
+                    </Card>
                 ))}
             </main>
             <footer>
