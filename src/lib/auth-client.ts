309cc33941d046960a7fa73e6cc842da00cc67f0
johnvesslyalti <altijohnvessly@gmail.com>
2025-12-06 15:57:23 +0530

    fix: change the auth client url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
- baseURL: "http://localhost:3000" // The base URL of your auth server
+ baseURL: "https://johnvesslyalti-cinescope.vercel.app" // The base URL of your auth server
 });
